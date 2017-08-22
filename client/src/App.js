7b6bdd76129909f4628008c9237a891ee784c028
Andrej Ruman <adorum@gmail.com>
2017-08-22 21:05:41 +0200

    Working but slow filtering

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react';
 import TreeView from './tree/TreeView';
 import Client from './client';
-import uniqueId from 'lodash/uniqueId';
+import {uniqueId, cloneDeep} from 'lodash';
 import './App.css';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
+    this.onSearchChanged = this.onSearchChanged.bind(this);
     this.state = {
-      data: null
+      data: null,
+      filteredData: null
     };
+  }
+
+  onSearchChanged(event) {
+    const clonedData = cloneDeep(this.state.data);
+    this.filter(clonedData, event.target.value);
+    this.setState({ filteredData: clonedData});
   }
 
   setNodeId(node) {
@@ -23,21 +31,30 @@ class App extends Component {
     })
   }
 
+  filter(node, searchText) {
+    node.children.filter(x => !this.filter(x, searchText)).forEach(a => {
+      var index = node.children.indexOf(a);
+      node.children.splice(index, 1);
+    });
+    return (node.name.toLowerCase().indexOf(searchText) !== -1) || (node.children.length > 0);
+  }
+
   componentDidMount() {
     Client.fetchCategories().then((data) => {
       this.setNodeId(data);
-      this.setState({data});
+      this.setState({ data });
     });
   }
 
   render() {
-    if (!this.state.data) {
+    if (this.state.data == null) {
       return <div>Loading...</div>;
     }
 
     return (
       <div className="App">
-        <TreeView data={this.state.data} expandToLevel={2}/>
+        <input type="text" placeholder="Search..." onChange={this.onSearchChanged}/>
+        <TreeView data={this.state.filteredData ||  this.state.data} expandToLevel={2}/>
       </div>
     );
   }
